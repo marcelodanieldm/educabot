3345c3a993bc8e7f5d549d8f35cc5974bf83f727
marcelodanieldm <danielmarcelo83@gmail.com>
2025-07-07 20:55:09 -0300

    adding baseUrl in cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,7 @@ module.exports = defineConfig({
       },
 
   e2e: {
+    baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/carrito.cy.js
+++ cypress/e2e/carrito.cy.js
@@ -1,7 +1,4 @@
 
-import loginPage from '../../support/pageObjects/loginPage';
-import HomePage from '../../support/pageObjects/HomePage';
-import CartPage from '../../support/pageObjects/CartPage';
 
 import loginPage from '../../support/pageObjects/loginPage';
 import HomePage from '../../support/pageObjects/HomePage';
@@ -19,19 +16,21 @@ describe('Tests de login', () => {
   it('LOGIN-001 Inicio de sesión exitoso con usuario válido (standard_user)', () => {
     loginPage.login('standard_user', 'secret_sauce');
 
+    });
+  
   });
 
-    it('LOGIN-002 Inicio de sesión fallido con contrasena incorrecta.', () => {
+    it('LOGIN-002 Inicio de sesión fallido con contrasena incorrecta', () => {
     loginPage.login('standard_user', 'password_incorrecta');
 
   });
 
-  it('LOGIN-003 Inicio de sesión fallido con usuario incorrecto/no registrado.', () => {
+  it('LOGIN-003 Inicio de sesión fallido con usuario incorrecto/no registrado', () => {
     loginPage.login('juan_cito', 'secreto_secreto_shh');
 
   });
 
-it('LOGIN-004 Caso Borde: Inicio de sesión fallido con campos vacíos (ambos).', () => {
+it('LOGIN-004 Caso Borde: Inicio de sesión fallido con campos vacíos (ambos)', () => {
     loginPage.login('', '');
 
   });
